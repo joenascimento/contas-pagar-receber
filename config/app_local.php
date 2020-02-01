8da3e326c06cdde419b66b5764f2bc7c11a5781a
Joe Pereira Nascimento <joenascimento2000@gmail.com>
2020-01-31 23:50:29 -0300

    db configuration

--- config/app_local.php
+++ config/app_local.php
@@ -47,7 +47,7 @@ return [
             'username' => 'root',
             'password' => 'linux',
 
-            'database' => 'dev',
+            'database' => 'dev_contas',
             /**
              * If not using the default 'public' schema with the PostgreSQL driver
              * set it here.
